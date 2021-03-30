b682a86675d4a8dcb67e6b36ad6d6fe20f01dfe9
Andreia Pereira <andreia.sofia.pereira@tecnico.ulisboa.pt>
2021-03-30 19:38:54 +0100

    Fixed KMP. Started Boyer-Moore preprocessing

--- project.c
+++ project.c
@@ -13,15 +13,23 @@
 
 #define TSIZE 32
 #define PSIZE 16
+#define ALPHSIZE 4
 
 /* --------------------------------------------------------- */
 char* saveNewString(char *S, int *S_cap, int *S_size);
 char* increaseMemory(char* S, int *S_cap);
-void buildPi(int *pi, char *P, int P_size);
 void decreaseMemory(char* S, int *S_cap);
+
 void naiveMatch(char *T, char *P);
+
 void KMPMatch(char *T, char *P, int T_size, int P_size);
-void BMMatch(char *T, char *P);
+void buildPi_KMP(int *pi, char *P, int P_size);
+
+void BMMatch(char *T, char *P, int T_size, int P_size);
+int charPosition_BM(char c);
+void buildR_BM(int *R, char* P, int P_size);
+void buildL_BM(int *L, char* P, int P_size);
+void buildNj_BM(int *Nj, char* P, int P_size);
 /* --------------------------------------------------------- */
 
 int main(int argc, char *argv[]){
@@ -72,7 +80,7 @@ int main(int argc, char *argv[]){
             case 'B':   /* Command for BM String Matching Algortihm */
                 if(checkT){
                     P = saveNewString(P, &P_cap, &P_size);
-                    
+                    BMMatch(T, P, T_size, P_size);
                 }
                 break;
             default:
@@ -136,7 +144,6 @@ void naiveMatch(char *T, char *P){
     int j = 0;                  /* iterates over P */
     char p = P[0];
     bool match = true;
-    bool alreadyMatched = false;
 
     while (T[i] != '\0'){
         j = 0;
@@ -150,11 +157,7 @@ void naiveMatch(char *T, char *P){
             p = P[j];
         }
         if((p == '\0') && (match == true)){
-            if(alreadyMatched)
-                printf(" ");
-            else
-                alreadyMatched = true;
-            printf("%d", i);
+            printf("%d ", i);
         }    
         i++;
     }
@@ -162,18 +165,17 @@ void naiveMatch(char *T, char *P){
 }
 
 void KMPMatch(char *T, char *P, int T_size, int P_size){
-/* num comparacoes = tamanho T + #chamadas da função de falha
+/* num comparacoes = tamanho T + #chamadas da funcao de falha
  se P e T acabam ate ao fim, nao chamar a funcao de falha*/
     
     int Pi[P_size+1];
   
-    buildPi(Pi, P, P_size); /* Pre processing P to build Pi array */
+    buildPi_KMP(Pi, P, P_size); /* Pre processing P to build Pi array */
    
     int q = 0;              /*Num of characters matched*/
     int i;
-    int n_comp = T_size+1;         /*Num of comparissions*/    
-    bool alreadyMatched = false;
-
+    int n_comp = 0;         /*Num of comparissions*/
+    bool endMatch = false;
     for(i = 0; i < T_size; i++){
         while( (q > 0) && (P[q] != T[i])){ /* While mismatch*/
             q = Pi[q];
@@ -184,37 +186,125 @@ void KMPMatch(char *T, char *P, int T_size, int P_size){
             q++;
         }
         if ( q == P_size){
-            if(alreadyMatched)
-                printf(" %d", i-P_size+1);
-            else{
-                printf("%d", i-P_size+1);
-                alreadyMatched = true;
-            }
+            n_comp++;
+            printf("%d ", i-P_size+1);
             q = Pi[q];
+            if(i == T_size-1) endMatch = true;
         } 
+        if(!endMatch) n_comp++;
     }
-    printf("\n%d \n", n_comp);
-    
+    printf("\n%d \n", n_comp);   
 }
 
-void buildPi(int *pi, char *P, int P_size){
+void buildPi_KMP(int *pi, char *P, int P_size){
 
     pi[1] = 0;
 
     int k = 0;
     int q;
-    for (q = 2; q <= P_size ; q++){
-        while((k > 0) && (P[k + 1] != P[q])){
+    for (q = 1; q < P_size ; q++){
+        while((k > 0) && (P[k] != P[q])){
             k = pi[k];
         }
 
-        if(P[k + 1] == P[q])
+        if(P[k] == P[q])
             k++;
-
-        pi[q] = k;
+        
+        pi[q+1] = k;
     }
 }
 
-void BMMatch(char *T, char *P){
+void buildR_BM(int *R, char* P, int P_size){
+    int i = P_size -1;
+    int num_chars = 0;
+    int j;
+    for(j = 0; j < ALPHSIZE; j++){
+        R[j] = -1 ;
+    }
+    while((num_chars != ALPHSIZE) && (i >= 0)){
+        if(R[charPosition_BM(P[i])] == -1)
+            R[charPosition_BM(P[i])] = i;
+        i--;
+    }   
+}
 
+/*
+Builds the L' array for the Boyer-Moore Algorithm
+*/
+void buildL_BM(int *L, char* P, int P_size){
+    int Nj[P_size];
+    buildNj_BM(Nj, P, P_size);
+    int j;
+    int i;
+    for(int i = 0; i<P_size; i++){
+        L[i] = 0;
+    }
+
+    for(j=0; j< P_size -1; j++){
+        i = P_size - Nj[j] - 1;
+        L[i] = j;
+    }
+     int c;
+    printf("L': [");
+    for (c = 0; c <P_size; c++){
+        printf(" %d", L[c]);
+    }
+    printf(" ]\n");
+}
+
+/*
+Builds the Nj array for the Boyer-Moore Algorithm
+*/
+void buildNj_BM(int *Nj, char* P, int P_size){
+    int i = P_size - 1;
+    int j;
+    for(j = 0; j < P_size; j++){
+        int k = j;
+        int length = 0;
+        while((k >= 0) && (P[k] == P[i])){
+            /*printf("Here! P[%d] = %c and P[%d] = %c\n", k, P[k], i, P[i]);*/
+            length++;
+            k--;
+            i--;
+        }
+        Nj[j] = length;
+        i = P_size - 1;
+    }
+    int c;
+    printf("Nj: [");
+    for (c = 0; c <P_size; c++){
+        printf(" %d", Nj[c]);
+    }
+    printf(" ]\n");
+}
+
+int charPosition_BM(char c){
+
+    switch (c)
+        {
+        case 'A':
+            return 0;
+            break;
+        case 'C':
+            return 1;
+            break;
+        case 'G':
+            return 2;
+            break;
+        case 'T':
+            return 3;
+            break;
+        default: /* Should never happen*/
+            return -1;
+            break;
+    }
+}
+
+void BMMatch(char *T, char *P, int T_size, int P_size){
+    int R[ALPHSIZE];
+    buildR_BM(R, P, P_size);
+    int L[P_size];
+    buildL_BM(L, P, P_size);
+    
+    
 }
